250d7ab4b67fa7a98d4cf693a723b990eabc3049
Kozak24 <okozak24@gmail.com>
2022-06-02 22:04:39 +0300

    Improving shell script, added pylint installation

--- ci/pylint.sh
+++ ci/pylint.sh
@@ -1,3 +1,11 @@
-python --version
+$echo Check python location and version
 which python
+python --version
+
+$echo Check python location and version
+which pip
 pip --version
+
+$echo Install pylint and check its version
+pip install pylint
+pylint --version
